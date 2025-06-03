3e11150a7310ae7966741d20a7c875175407efb4
Elei Mathew Tatad <elei.mathew.tatad@accenture.com>
2025-06-03 02:44:08 +0000

    main

--- webapp/controller/MainView.controller.js
+++ webapp/controller/MainView.controller.js
@@ -3,8 +3,17 @@ sap.ui.define([
 ], (Controller) => {
     "use strict";
 
-    return Controller.extend("com.acn.training.exer1tatad.controller.MainView", {
+    return Controller.extend("com.acn.training.firstproject.controller.mainview", {
         onInit() {
+        },
+        onClearPress: function () {
+            // Clear text fields
+            this.byId("nameInput").setValue("");
+            this.byId("streetInput").setValue("");
+            this.byId("cityInput").setValue("");
+        
+            // Clear ComboBox selection
+            this.byId("box0").setSelectedKey(null);
         }
     });
 });
